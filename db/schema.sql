39aee7b14de9165fee7d38a69a7d804b50cb2683
Roman Pogorelov <rmk140987@gmail.com>
2021-09-26 11:46:18 +0300

    1. Авторизация через JDBC [#2094]

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,23 @@ DROP TABLE IF EXISTS accident_rule;
 DROP TABLE IF EXISTS accident;
 DROP TABLE IF EXISTS accident_type;
 DROP TABLE IF EXISTS rule;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS authorities;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    username VARCHAR(50)  NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    enabled  boolean default true,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE IF NOT EXISTS authorities
+(
+    username  VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users (username)
+);
 
 CREATE TABLE IF NOT EXISTS rule
 (
